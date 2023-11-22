905ed46033120c379f762c853f77a4273e9c97f7
Yamilex Jimenez <141197439+yamij23@users.noreply.github.com>
2023-11-22 05:48:25 -0700

    Add files via upload

---
+++ public/src/accounts.js
@@ -0,0 +1,57 @@
+function findAccountById(accounts, id) {
+  // YOUR SOLUTION HERE
+  let foundId = accounts.find((account) => account.id === id);
+  return foundId;
+  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+}
+
+function sortAccountsByLastName(accounts) {
+  // YOUR SOLUTION HERE
+  accounts.sort((accountA, accountB) =>
+  accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1
+);
+  return accounts;
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+}
+
+function getAccountFullNames(accounts) {
+  // YOUR SOLUTION HERE
+  
+  let result =  accounts.map(account => account.name.first + ' ' + account.name.last);
+  console.log("result",result);
+  return result;
+}
+
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
